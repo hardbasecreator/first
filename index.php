2cd262f141f763f090498137dcaf9ae04f4dc50f
hardbase <hardbase.work@gmail.com>
2016-02-09 21:57:17 +0200

    second commit

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 header ("Content-Type: text/html; charset=utf-8");
 echo "<!DOCTYPE html>
 <html>
@@ -8,6 +8,11 @@ echo "<!DOCTYPE html>
 </head>
 <body style=\"background: url(fon.png) top left repeat-x\">
 <center>
+
+
+var_dump($alias);
+
+
 <br><br><br><div style=\"width: 600px;\"><span style=\"font-size: 32px; color: green; font-family: Arial, Verdana; text-shadow: 0 1px 0 #fff\">Добро пожаловать в Open Server!</span>
 <br><br><br><span style=\"font-size: 32px; color: #333; font-family: Verdana, Arial;\">Он работает ;-)</span>
 <br><img src=\"st.png\" style=\"margin: 40px 0\"><br><a href=\"http://open-server.ru/docs/\" style=\"font-size: 24px; color: #048acd; font-family: Arial;\">Руководство пользователя</a></span><br><br><br></div>
